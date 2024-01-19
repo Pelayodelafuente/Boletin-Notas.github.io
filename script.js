debddc58cab3410a8d8e4004987944eecd9b3531
Pelayodelafuente <delafuentepelayo@gmail.com>
2024-01-19 10:38:33 +0100

    Subida de archivos

---
+++ script.js
@@ -0,0 +1,169 @@
+window.onload = function() {
+
+
+    async function cargarDatos(){
+
+        const response = await fetch("../alumnos.json");
+        const alumnos = await response.json();
+        procesarAlumnos(alumnos);
+    }
+
+    
+
+    function procesarAlumnos(alumnos){
+
+        const verBoletines = document.getElementById('verBoletines');
+        verBoletines.addEventListener('click', mostrarBoletines);
+
+        function mostrarBoletines(){
+
+        const fichaAlumno = document.getElementById('fichaAlumno');
+        fichaAlumno.innerHTML='';
+
+        // const contenedor = document.createElement('div');
+        // contenedor.className='contenedor';
+
+
+        fichaAlumno.innerHTML='';
+        
+
+        for (const alumno of alumnos){
+           
+            const div = document.createElement('div');
+            div.className = 'alumno';
+
+
+            const foto = document.createElement('img');
+            foto.src = `./imagenes/${alumno.foto}`;
+            foto.className='foto';
+            div.appendChild(foto);
+
+            const nombreCompleto = document.createElement('h2');
+            nombreCompleto.textContent = `${alumno.nombre} ${alumno.apellidos}`;
+            div.appendChild(nombreCompleto);
+            
+
+            const asignaturas = document.createElement('div');
+            asignaturas.className='notas';
+
+            
+            const mate= document.createElement('p');
+            mate.innerHTML = `Matemáticas: <span>${alumno.matematicas}</span>`;
+            asignaturas.appendChild(mate);
+
+            const lengua = document.createElement('p');
+            lengua.innerHTML = `Lengua: <span>${alumno.lengua}</span>`;
+            asignaturas.appendChild(lengua);
+
+            const ingles = document.createElement('p');
+            ingles.innerHTML = `Ingles: <span>${alumno.ingles}</span>`;
+            asignaturas.appendChild(ingles);
+
+
+            div.appendChild(asignaturas);
+            document.body.appendChild(fichaAlumno);
+            fichaAlumno.appendChild(div);
+            
+            
+        }
+
+
+
+    }
+
+       const seleccionaAsignatura = document.getElementById('asignaturas');
+       const verNotas = document.getElementById('ver-notas');
+       const fichaAlumno = document.getElementById('fichaAlumno');
+
+
+       verNotas.addEventListener('click', mostrarNotas);
+       function mostrarNotas(){
+
+
+        const materiaSeleccionada = seleccionaAsignatura.value;
+       
+        fichaAlumno.innerHTML='';
+
+        
+
+        for (const alumno of alumnos){
+
+
+            const div = document.createElement('div');
+            div.className = 'alumno';
+
+            const foto = document.createElement('img');
+            foto.src = `./imagenes/${alumno.foto}`;
+            foto.className='foto';
+            div.appendChild(foto);
+
+            const nombreCompleto = document.createElement('h2');
+            nombreCompleto.textContent = `${alumno.nombre} ${alumno.apellidos}`;
+            div.appendChild(nombreCompleto);
+
+            const nota = document.createElement('p');
+            nota.textContent=`${materiaSeleccionada}: ${alumno.matematicas}`;
+            div.appendChild(nota);
+
+            fichaAlumno.appendChild(div);
+
+        
+        }
+
+    
+       }
+
+
+
+
+       const notaMedia = document.getElementById('nota-media');
+       notaMedia.addEventListener('click', mostrarMedia);
+
+       function mostrarMedia(){
+
+        fichaAlumno.innerHTML='';
+
+        for (const alumno of alumnos){
+
+            const div = document.createElement('div');
+            div.className = 'alumno';
+
+            const foto = document.createElement('img');
+            foto.src = `./imagenes/${alumno.foto}`;
+            foto.className='foto';
+            div.appendChild(foto);
+
+            const nombreCompleto = document.createElement('h2');
+            nombreCompleto.textContent = `${alumno.nombre} ${alumno.apellidos}`;
+            div.appendChild(nombreCompleto);
+
+            const notaMediaAlumno = document.createElement('p');
+            const media = (alumno.lengua + alumno.matematicas + alumno.ingles) / 3;
+            notaMediaAlumno.textContent = `Nota media: ${media.toFixed(2)}`;
+            div.appendChild(notaMediaAlumno);
+
+            fichaAlumno.appendChild(div);
+
+        }
+
+       }
+
+    
+        
+
+       
+
+    }
+
+    
+
+    cargarDatos();
+
+
+
+
+
+
+}
+
+ 
---
+++ scriptpdf.js
@@ -0,0 +1,162 @@
+window.onload = function() {
+
+    async function cargarDatos(){
+
+        const response = await fetch("../alumnos.json");
+        const alumnos = await response.json();
+        procesarAlumnos(alumnos);
+    }
+
+    function procesarAlumnos(alumnos){
+
+       
+
+        for (const alumno of alumnos){
+
+        const encabezado=document.createElement('div');
+        encabezado.className="encabezado";
+
+        const logo = document.createElement('img');
+        logo.className="logo";
+        logo.src="./imagenes/logo.png";
+
+        const h1 = document.createElement('h1');
+        h1.innerHTML=`Centro de estudios <span>La Laboral</span>`;
+
+        encabezado.appendChild(logo);
+        encabezado.appendChild(h1);
+        
+
+        const alumnoGeneral = document.getElementById('alumno');
+        const alumnoFicha = document.createElement('div');
+        alumnoFicha.className='fichasAlumno';
+
+        alumnoFicha.appendChild(encabezado);
+
+        const identidad = document.createElement('div');
+        identidad.className="alumnoInd";
+        const foto = document.createElement('img');
+        foto.src=`./imagenes/${alumno.foto}`;
+        foto.className='foto';
+        identidad.appendChild(foto);
+        alumnoFicha.appendChild(identidad);
+        
+        
+        
+
+        const h2 = document.createElement('h2');
+        h2.textContent= `${alumno.nombre} ${alumno.apellidos}`;
+        identidad.appendChild(h2);
+
+
+        alumnoFicha.appendChild(identidad);
+    
+
+
+        const divNotas = document.createElement('div');
+        divNotas.className='divNotas';
+
+
+        const divNombreMateria = document.createElement('div');
+        divNombreMateria.className='nombreMateria';
+
+        const mate = document.createElement('h2');
+        mate.textContent="Matemáticas:";
+
+        const lengua = document.createElement('h2');
+        lengua.textContent="Lengua:";
+
+        const ingles = document.createElement('h2');
+        ingles.textContent="Ingles:";
+
+        
+        const divNotaMateria = document.createElement('div');
+
+        const notaMate = document.createElement('h2');
+        notaMate.innerHTML=`${alumno.matematicas}`;
+
+        const notaLengua = document.createElement('h2');
+        notaLengua.innerHTML=`${alumno.lengua}`;
+
+        const notaIngles = document.createElement('h2');
+        notaIngles.innerHTML=`${alumno.ingles}`;
+
+        
+
+        
+
+       
+
+       
+        divNombreMateria.appendChild(mate);
+        divNombreMateria.appendChild(lengua);
+        divNombreMateria.appendChild(ingles);
+        divNotas.appendChild(divNombreMateria);
+
+        
+
+        divNotaMateria.appendChild(notaMate);
+        divNotaMateria.appendChild(notaLengua);
+        divNotaMateria.appendChild(notaIngles);
+        divNotas.appendChild(divNotaMateria);
+
+       
+        alumnoFicha.appendChild(divNotas);
+        
+
+        alumnoGeneral.appendChild(alumnoFicha);
+        
+        }
+
+
+        let creaPDF = document.getElementById("imprimir");
+        creaPDF.addEventListener("click", imprimeNotas);
+    
+        function imprimeNotas(){
+    
+            let boletines = document.querySelector("#alumno");
+    
+            html2pdf()
+                .set({
+                    // margin: 0,
+                    filename: 'Boletin notas.pdf',
+                    image: {
+                        type: 'jpeg',
+                        quality:0.98
+                    },
+                    html2canvas:{
+                        scale:1,
+                        letterRendering: true,
+                    },
+                    jsPDF:{
+                        unit:"in",
+                        format: "a3",
+                        orientation: 'portrait'
+                    }
+                })
+                .from(boletines)
+                .save();
+        }
+
+        
+
+        
+    }
+    
+
+
+    cargarDatos();
+
+
+   
+
+   
+
+   
+
+  
+  
+
+
+    
+}
